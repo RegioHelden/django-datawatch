c6f76d52429d77b0d0c362fa9fd59aa518aa6ded
Jens Nistler <opensource@jensnistler.de>
2016-09-04 11:31:17 +0200

    release 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     name='django-datawatch',
     packages=['django_datawatch'],
-    version='0.1.0',
+    version='0.1.1',
     description='Django Datawatch runs automated data checks in your Django installation',
     author='Jens Nistler <opensource@jensnistler.de>, Bogdan Radko',
     author_email='opensource@regiohelden.de',
