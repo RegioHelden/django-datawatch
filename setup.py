8bf4ae1cba7c6adb1349a4314015f4d88a54e309
Mounir Messelmeni <messelmeni.mounir@gmail.com>
2020-11-13 18:18:37 +0100

    Bump version: 2.2.1 → 2.2.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def long_description():
 setup(
     name='django-datawatch',
     packages=find_packages(exclude=['example*']),
-    version='2.2.1',
+    version='2.2.2',
     description='Django Datawatch runs automated data checks in your Django installation',
     author='Jens Nistler <opensource@jensnistler.de>, Bogdan Radko <bogdan.radko@regiohelden.de>',
     author_email='opensource@regiohelden.de',
@@ -23,7 +23,7 @@ setup(
     long_description_content_type='text/markdown',
     install_requires=[
         'celery>=4.3.0',
-        'Django>=2.2.16',
+        'Django>=2.2.26',
         'django-bootstrap3>=14.1.0',
         'django-extensions>=2.2.5',
         'django-model-utils>=3.2.0',
