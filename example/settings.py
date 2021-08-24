b3e3c1a0b9db7b434ef75e7766ef461088e86b5f
Jens Nistler <jens.nistler@regiohelden.de>
2021-08-24 14:54:49 +0200

    renovate test environment

--- example/settings.py
+++ example/settings.py
@@ -50,7 +50,6 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
@@ -184,3 +183,5 @@ SESSION_SERIALIZER = 'django.contrib.sessions.serializers.PickleSerializer'
 BOOTSTRAP3 = {'horizontal_label_class': 'col-md-2', 'horizontal_field_class': 'col-md-10', 'success_css_class': ''}
 
 DJANGO_DATAWATCH_BACKEND = 'django_datawatch.backends.synchronous'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
