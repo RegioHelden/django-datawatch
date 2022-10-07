4e167595fb6577f339b6b2f4ef53bd47a86329f3
Jens Nistler <opensource@jennsistler.de>
2022-10-07 09:49:06 +0200

    fix remaining django 4.0 compatibility issues

--- django_datawatch/urls.py
+++ django_datawatch/urls.py
@@ -1,13 +1,13 @@
 # -*- coding: UTF-8 -*-
-from django.conf.urls import url
+from django.urls import path
 
 from django_datawatch import views
 
 urlpatterns = [
-    url(r'^$', views.DashboardView.as_view(), name='django_datawatch_index'),
-    url(r'^result/(?P<pk>\d+)/$', views.ResultView.as_view(), name='django_datawatch_result'),
-    url(r'^result/(?P<pk>\d+)/acknowledge/$', views.ResultAcknowledgeView.as_view(),
+    path('', views.DashboardView.as_view(), name='django_datawatch_index'),
+    path('result/<int:pk>/', views.ResultView.as_view(), name='django_datawatch_result'),
+    path('result/<int:pk>/acknowledge/', views.ResultAcknowledgeView.as_view(),
         name='django_datawatch_result_acknowledge'),
-    url(r'^result/(?P<pk>\d+)/config/$', views.ResultConfigView.as_view(), name='django_datawatch_result_config'),
-    url(r'^result/(?P<pk>\d+)/refresh/$', views.ResultRefreshView.as_view(), name='django_datawatch_result_refresh'),
+    path('result/<int:pk>/config/', views.ResultConfigView.as_view(), name='django_datawatch_result_config'),
+    path('result/<int:pk>/refresh/', views.ResultRefreshView.as_view(), name='django_datawatch_result_refresh'),
 ]
