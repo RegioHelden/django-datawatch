40d7cae3f98c7c9634593b714accf653413242ab
Richard Ackon <richard.ackon@regiohelden.de>
2023-04-19 09:04:32 +0000

    added shared task for datawatch results and check executions cleanup

--- django_datawatch/management/commands/datawatch_clean_up.py
+++ django_datawatch/management/commands/datawatch_clean_up.py
@@ -2,21 +2,13 @@ import logging
 
 from django.core.management.base import BaseCommand
 
-from django_datawatch.models import Result, CheckExecution
+from django_datawatch.tasks import datawatch_cleanup
 
 logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
-    help = 'Delete all results and executions for removed checks.'
+    help = "Delete all results and executions for removed checks."
 
     def handle(self, *args, **options):
-        results = Result.objects.ghost_results()
-        check_names = list(results.distinct().values_list('slug', flat=True))
-        results.delete()
-        logger.info('%d results have been deleted:\n%s', len(check_names), '\n'.join(check_names))
-
-        check_executions = CheckExecution.objects.ghost_executions()
-        check_names = list(check_executions.distinct().values_list('slug', flat=True))
-        check_executions.delete()
-        logger.info('%d check executions have been deleted:\n%s', len(check_names), '\n'.join(check_names))
+        datawatch_cleanup()
