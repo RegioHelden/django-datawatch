2955e4a8a281bb36a93d2d01cdd5506f3cdbc34b
Jens Nistler <opensource@jensnistler.de>
2023-04-20 10:19:18 +0200

    claim support for python 3.11 and django 4.2

--- setup.py
+++ setup.py
@@ -34,8 +34,7 @@ setup(
     long_description_content_type='text/markdown',
     install_requires=[
         'celery>=5.0.0',
-        'Django>=3.2,<4.2',
-        'django-bootstrap3>=21.1',
+        'Django>=3.2,<5.0',
         'django-extensions>=3.0.0',
         'django-model-utils>=4.2.0',
         'python-dateutil>=2.8.0',
@@ -51,6 +50,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Development Status :: 5 - Production/Stable",
         "Environment :: Web Environment",
         "Intended Audience :: Developers",
